09505a80d082f0648615e30f57e33ac1a3189e0e
Juan Jacobs <juanmjacobs@gmail.com>
2020-06-21 14:38:35 -0300

    wine quality regressor

---
+++ wine-quality/use-regressor.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+ 
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.externals import joblib 
+# 3. Load red wine data.
+dataset_url = 'http://mlr.cs.umass.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv'
+data = pd.read_csv(dataset_url, sep=';')
+ 
+# 4. Split data into training and test sets
+y = data.quality
+X = data.drop('quality', axis=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, 
+                                                    test_size=0.2, 
+                                                    random_state=123, 
+                                                    stratify=y)
+ 
+clf2 = joblib.load('rf_regressor.pkl')
+ 
+# Predict data set using loaded model
+pred = clf2.predict(X_test)
+print(pred)
+print(r2_score(y_test, pred))
+print(mean_squared_error(y_test, pred))
